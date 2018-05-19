d440120cdcb3f0828cff18f0b9c20bc3b930dc35
Esther Weyinmi Koloh <kolohw@gmail.com>
2018-05-19 15:19:23 +0200

    activity_company.xml renamed to tour_list.xml

--- app/src/main/java/com/example/koloh/tourguideapp/HotelsActivity.java
+++ app/src/main/java/com/example/koloh/tourguideapp/HotelsActivity.java
@@ -2,16 +2,42 @@ package com.example.koloh.tourguideapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class HotelsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
-        setContentView ( R.layout.activity_hotels );
+        setContentView ( R.layout.tour_list );
 
         // Create a back Action arrow to go back to main Activity
-        getSupportActionBar ().setTitle ( R.string.title_hotels );
+        getSupportActionBar ().setTitle ( R.string.title_companies );
         getSupportActionBar ().setDisplayHomeAsUpEnabled ( true );
+
+// Create a list of words
+        ArrayList<Tour> tours = new ArrayList<Tour> ();
+        tours.add ( new Tour ( "PROSOZ Herten GmbH", "Ewaldstraße 261, 45699 Herten" ) );
+        tours.add ( new Tour ( "Johrendt-Bautenschutz GmbH", "Grimmstraße 6, 45701 Herten" ) );
+        tours.add ( new Tour ( "Spedition Wilhelm Mazurek GmbH", "Industriestraße 12B, 45699 Herten" ) );
+        tours.add ( new Tour ( "Autohaus Blendorf GmbH Mazda", "Schlägel-und-Eisen-Straße 36, 45701 Herten" ) );
+        tours.add ( new Tour ( "Imperial Industrial Logistics GmbH", "Carl-Bosch-Straße 2-6, 45699 Herten" ) );
+        tours.add ( new Tour ( "Klaeser Internationale Fachspedition und Fahrzeugbau GmbH", "Hohewardstraße 333, 45699 Herten" ) );
+        tours.add ( new Tour ( "Dataserv GmbH", "Hohewardstraße 329, 45699 Herten" ) );
+        tours.add ( new Tour ( "Herta GmbH", "Westerholter Str. 750, 45701 Herten" ) );
+        tours.add ( new Tour ( "Coca-Cola European Partners Germany GmbH", "Paschenbergstraße 30, 45699 Herten" ) );
+        tours.add ( new Tour ( "ISRA Surface Vision GmbH", "Albert-Einstein-Allee 36-40, 45699 Herten" ) );
+
+        // Created an {@link ArrayAdapter}, whose data source is a list of Strings.
+        TourAdapter adapter =
+                new TourAdapter ( this, tours );
+
+        // Finding the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        ListView listView = (ListView) findViewById ( R.id.list );
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} to enable {@link ListView} display list items.
+        listView.setAdapter ( adapter );
     }
 }
--- app/src/main/java/com/example/koloh/tourguideapp/Tour.java
+++ app/src/main/java/com/example/koloh/tourguideapp/Tour.java
@@ -1,4 +1,44 @@
 package com.example.koloh.tourguideapp;
 
+/**
+ * {@link Tour} represents more images that users want to know and visit.
+ * It contains a default name of the places  and images for the Tour places.
+ */
+
 public class Tour {
+    /**
+     * Default translation for the word
+     */
+    private String mDefaultName;
+
+    /**
+     * Miwok translation for the word
+     */
+    private String mPlaceAddress;
+
+    /**
+     * Create a new Tour object.
+     *
+     * @param defaultName  the name of the Tour Place
+     * @param placeAddress the address of the place
+     */
+    public Tour(String defaultName, String placeAddress) {
+        mDefaultName = defaultName;
+        mPlaceAddress = placeAddress;
+    }
+
+    /**
+     * Get the default translation of the word.
+     */
+    public String getDefaultName() {
+        return mDefaultName;
+    }
+
+    /**
+     * Get the Miwok translation of the word.
+     */
+    public String getPlaceAddress() {
+        return mPlaceAddress;
+    }
 }
+

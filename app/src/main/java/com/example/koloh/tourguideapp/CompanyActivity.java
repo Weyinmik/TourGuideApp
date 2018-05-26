4457876b1b17594173cb7298f8a4ecf09ed183b9
Esther Weyinmi Koloh <kolohw@gmail.com>
2018-05-26 10:59:04 +0200

    CompanyActivity updated

--- app/src/main/java/com/example/koloh/tourguideapp/CompanyActivity.java
+++ app/src/main/java/com/example/koloh/tourguideapp/CompanyActivity.java
@@ -1,14 +1,7 @@
 package com.example.koloh.tourguideapp;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-
-import java.util.ArrayList;
+import android.os.Bundle;
 
 public class CompanyActivity extends AppCompatActivity {
 
@@ -16,7 +9,10 @@ public class CompanyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
-        setContentView ( R.layout.tour_list );
+        setContentView ( R.layout.tour_category );
+        getSupportFragmentManager ().beginTransaction ()
+                .replace ( R.id.tour_container, new CompanyFragment () )
+                .commit ();
 
         // Create a back Action arrow to go back to main Activity
         getSupportActionBar ().setTitle ( R.string.title_companies );

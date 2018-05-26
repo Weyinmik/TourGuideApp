d0a4a2a794f3df58f9f934fa106950e9fae99a1d
Esther Weyinmi Koloh <kolohw@gmail.com>
2018-05-26 19:49:15 +0200

    Updated the FragmentPagerAdapter class to display the various Activities.

--- app/src/main/java/com/example/koloh/tourguideapp/MainActivity.java
+++ app/src/main/java/com/example/koloh/tourguideapp/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/koloh/tourguideapp/TourFragmentPagerAdapter.java
+++ app/src/main/java/com/example/koloh/tourguideapp/TourFragmentPagerAdapter.java
@@ -1,5 +1,6 @@
 package com.example.koloh.tourguideapp;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
@@ -7,5 +8,25 @@ public class TourFragmentPagerAdapter extends FragmentPagerAdapter {
 
     public TourFragmentPagerAdapter(FragmentManager fragmentManager) {
         super ( fragmentManager );
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new DiscoverFragment ();
+        } else if (position == 1) {
+            return new CompanyFragment ();
+        } else if (position == 2) {
+            return new HotelsFragment ();
+        } else if (position == 3) {
+            return new SportFragment ();
+        } else {
+            return new RestaurantsFragment ();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 5;
     }
 }
